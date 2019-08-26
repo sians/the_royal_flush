19cf53dea2c6846ba9b0e8d63eaa1e2270780ab2
Jonathan Spielberg <jonathan.spielberg@icloud.com>
2019-08-26 18:37:20 +0100

    Created toilets controller

---
+++ app/controllers/toilets_controller.rb
@@ -0,0 +1,46 @@
+class ToiletsController < ApplicationController
+  before_action :fetch_toilet, only: %i[show destroy edit]
+
+  def index
+    @toilets = Toilet.all
+  end
+
+  def show
+  end
+
+  def new
+    @toilet = Toilet.new
+  end
+
+  def create
+    @toilet = Toilet.new(toilet_params)
+    if @toilet.save
+      redirect_to show_path(@toilet)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @toilet.update(toilet_params)
+    @toilet.save
+  end
+
+  def destroy
+    @toilet.destroy
+    redirect_to toilets_path
+  end
+
+  private
+
+  def toilet_params
+    params.require(:toilet).permit(:address, :description, :images)
+  end
+
+  def fetch_toilet
+    @toilet = Toilet.find(params[:id])
+  end
+end
