ff094c49423fc26d3ea36be10cac34842755627d
Dinis Brito <dinis_brito@netcabo.pt>
2019-08-27 12:27:00 +0100

    added name to toilet

--- app/models/toilet.rb
+++ app/models/toilet.rb
@@ -3,6 +3,7 @@ class Toilet < ApplicationRecord
   has_many :bookings
   has_many :reviews, through: :bookings
 
+  validates :name, presence: true, allow_blank: false
   validates :address, presence: true, allow_blank: false
   validates :description, presence: true
   validates :images, presence: true
