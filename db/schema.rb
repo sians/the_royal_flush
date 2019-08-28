b45d3f9ce30cf91137b3fc9578e7a7f2ad48742d
Vincent Plassard <plassard.vincent@gmail.com>
2019-08-28 13:17:29 +0100

    starting changes on the homepage

--- db/schema.rb
+++ db/schema.rb
@@ -74,4 +74,5 @@ ActiveRecord::Schema.define(version: 2019_08_28_093515) do
   add_foreign_key "bookings", "toilets"
   add_foreign_key "bookings", "users"
   add_foreign_key "reviews", "bookings"
+  add_foreign_key "toilets", "users", column: "owner_id"
 end
