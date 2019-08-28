2ef4e872c53bb113692d94a13c527691a5cff50f
Jonathan Spielberg <jonathan.spielberg@icloud.com>
2019-08-28 16:01:56 +0100

    Seed changes. Did not work as aspected

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -1,7 +1,8 @@
 import places from 'places.js';
 
 const initAutocomplete = () => {
-  const addressInput = document.querySelector('.address-input');
+  const addressInput = document.querySelector('.address-input') ||
+                       document.getElementById("toilet_address");
   if (addressInput) {
     places({ container: addressInput });
   }
