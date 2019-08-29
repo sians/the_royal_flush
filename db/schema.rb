e87cae1f970f912d3e8ff6dad5e5d821a0468996
Jonathan Spielberg <jonathan.spielberg@icloud.com>
2019-08-29 10:53:10 +0100

    Local changes db/schema ??

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_28_114525) do
+ActiveRecord::Schema.define(version: 2019_08_28_135849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,10 +51,10 @@ ActiveRecord::Schema.define(version: 2019_08_28_114525) do
     t.bigint "owner_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "price"
     t.string "name"
     t.float "latitude"
     t.float "longitude"
+    t.integer "price"
     t.index ["owner_id"], name: "index_toilets_on_owner_id"
   end
 
